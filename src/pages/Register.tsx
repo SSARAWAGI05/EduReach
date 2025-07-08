3153b3a7c9f7cb7275af3ae85f48b1c5cd6468f6
Shubam Sarawagi <sarawagishubam@gmail.com>
2025-07-08 12:32:59 +0530

    FIX

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -53,7 +53,7 @@ const Register: React.FC = () => {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: 'https://learnwithrishika.vercel.app/', // ✅ adjust if needed
+        redirectTo: 'https://ofxinmglsqsbyzsiofcy.supabase.co/auth/v1/callback', // ✅ adjust if needed
       },
     });
   };
